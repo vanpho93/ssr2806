915e1fcc52d548f4ad55cd82f0ee082a34606b0f
Van Pho Nguyen <vanpho02@gmail.com>
2017-06-30 14:16:42 +0700

    init

---
+++ src/app.js
@@ -0,0 +1,7 @@
+const React = require('react');
+const ReactDOM = require('react-dom');
+
+ReactDOM.render(
+    <a href="Yahoo.com">Yahoo!</a>,
+    document.getElementById('root') // eslint-disable-line
+);
