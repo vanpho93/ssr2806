4fbd13b70a4f8c80718178e8044b5afee05c45cf
Van Pho Nguyen <vanpho02@gmail.com>
2017-07-03 14:39:45 +0700

    abcd

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,17 +1,43 @@
 import React, { Component } from 'react';
 
 export default class Note extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isUpdating: false
+        };
+        this.toggleIsUpdating = this.toggleIsUpdating.bind(this);
+        this.onRemoveNote = this.onRemoveNote.bind(this);
+    }
+
     onRemoveNote() {
         this.props.onRemove(this.props.index);
     }
 
+    getDescJSX() {
+        const { desc } = this.props.task;
+        const { isUpdating } = this.state;
+        const updatingJSX = (
+            <div>
+                <input defaultValue={desc} type="text" /> 
+                <br /><br />
+            </div> 
+        );
+        return isUpdating ? updatingJSX : <p>{desc}</p>;
+    }
+
+    toggleIsUpdating() {
+        this.setState({ isUpdating: !this.state.isUpdating });
+    }
+
     render() {
-        const { subject, desc } = this.props.task;
+        const { subject } = this.props.task;
         return (
             <div>
                 <h3>{ subject }</h3>
-                <p>{ desc }</p>
-                <button onClick={this.onRemoveNote.bind(this)}>Delete</button>
+                { this.getDescJSX() }
+                <button onClick={this.onRemoveNote}>Delete</button>
+                <button onClick={this.toggleIsUpdating}>Update</button>
             </div>
         );
     }
