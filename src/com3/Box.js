858343ec7b6af243e6491050f3bb309aeb375043
Van Pho Nguyen <vanpho02@gmail.com>
2017-07-03 15:37:16 +0700

    xong

--- src/com3/Box.js
+++ src/com3/Box.js
@@ -7,6 +7,7 @@ export default class Box extends Component {
         super(props);
         this.setInfo = this.setInfo.bind(this);
         this.up = this.up.bind(this);
+        this.down = this.down.bind(this);
     }
 
     setInfo(infoInstance) {
@@ -18,12 +19,22 @@ export default class Box extends Component {
         this.info.setState({ value: newValue });
     }
 
+    // down() {
+    //     const info = this.refs.MY_INFO;
+    //     const newValue = info.state.value - 1;
+    //     info.setState({ value: newValue });
+    // }
+    down() {
+        const newValue = this.info2.state.value - 1;
+        this.info2.setState({ value: newValue });
+    }
+
     render() {
         return (
             <div>
                 <button onClick={this.up}>+</button>
-                <button>-</button>
-                <Info setInfo={this.setInfo} />
+                <button onClick={this.down}>-</button>
+                <Info setInfo={this.setInfo} ref={info => { this.info2 = info; }} />
             </div>
         );
     }
