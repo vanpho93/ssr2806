6c13c46ed272208dc0320ed9c9f04965cb16f7db
Van Pho Nguyen <vanpho02@gmail.com>
2017-07-03 15:28:07 +0700

    com

--- src/com3/Box.js
+++ src/com3/Box.js
@@ -2,12 +2,28 @@ import React, { Component } from 'react';
 import Info from './Info';
 
 export default class Box extends Component {
+
+    constructor(props) {
+        super(props);
+        this.setInfo = this.setInfo.bind(this);
+        this.up = this.up.bind(this);
+    }
+
+    setInfo(infoInstance) {
+        this.info = infoInstance;
+    }
+
+    up() {
+        const newValue = this.info.state.value + 1;
+        this.info.setState({ value: newValue });
+    }
+
     render() {
         return (
             <div>
-                <button>+</button>
+                <button onClick={this.up}>+</button>
                 <button>-</button>
-                <Info />
+                <Info setInfo={this.setInfo} />
             </div>
         );
     }
--- src/com3/Info.js
+++ src/com3/Info.js
@@ -6,6 +6,7 @@ export default class Info extends Component {
         this.state = { 
             value: 1
         };
+        this.props.setInfo(this);
     }
     render() {
         return (
